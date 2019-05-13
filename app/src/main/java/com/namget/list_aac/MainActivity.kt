d7ebf2bdfe340a2957c0e547fb1f7a4c5f963ac5
leejaewoo <yeboykr@naver.com>
2019-05-13 13:32:43 +0900

    first commit

---
+++ app/src/main/java/com/namget/list_aac/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.namget.list_aac
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
